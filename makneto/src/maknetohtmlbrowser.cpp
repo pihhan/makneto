94224286dac7d2e7497d9854b5f5ca42f9d9d939
jreznik <jreznik@8464b4cf-2e42-0410-a059-b11125635967>
2008-04-17 10:48:27 +0000

    Home tab - again sf net mainpage
    git-svn-id: https://makneto.svn.sourceforge.net/svnroot/makneto/trunk@50 8464b4cf-2e42-0410-a059-b11125635967

--- makneto/src/maknetohtmlbrowser.cpp
+++ makneto/src/maknetohtmlbrowser.cpp
@@ -11,18 +11,14 @@
 #include "kurl.h"
 
 #include <QtGui/QVBoxLayout>
-#include <QtGui/QFrame>
 
 MaknetoHTMLBrowser::MaknetoHTMLBrowser(QWidget *)
 {
 	mainLayout = new QVBoxLayout;
 
-	//QFrame *frame = new QFrame;
-
 	KUrl url("http://makneto.sf.net/");
 	KHTMLPart *homeTabBrowser = new KHTMLPart;
 	homeTabBrowser->openUrl(url);
-	//homeTabBrowser->view()->resize(500, 500);
 
 	mainLayout->addWidget(homeTabBrowser->view());
 
