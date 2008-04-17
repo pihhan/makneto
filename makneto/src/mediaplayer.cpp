b7fd13853fe6dd88d08e00f56a3cf95e25e2cd90
jreznik <jreznik@8464b4cf-2e42-0410-a059-b11125635967>
2008-04-17 10:47:38 +0000

    Media player class - only declaration!
    git-svn-id: https://makneto.svn.sourceforge.net/svnroot/makneto/trunk@49 8464b4cf-2e42-0410-a059-b11125635967

---
+++ makneto/src/mediaplayer.cpp
@@ -0,0 +1,12 @@
+/*
+ * mediaplayer.cpp
+ *
+ * Copyright (C) 2008 Jaroslav Reznik <rezzabuh@gmail.com>
+ */
+
+#include "mediaplayer.h"
+
+MediaPlayer::MediaPlayer(QWidget * parent) : QDialog(parent)
+{
+
+}
---
+++ makneto/src/mediaplayer.h
@@ -0,0 +1,19 @@
+/*
+ * mediaplayer.h
+ *
+ * Copyright (C) 2008 Jaroslav Reznik <rezzabuh@gmail.com>
+ */
+
+#ifndef MEDIAPLAYER_H
+#define MEDIAPLAYER_H
+
+#include <QtGui/QDialog>
+
+class MediaPlayer: public QDialog
+{
+public:
+	MediaPlayer(QWidget * parent = 0);
+
+};
+
+#endif //MEDIAPLAYER_H
